64492eff96cc8c662423e7a87c66e747c709524a
Beatriz Perez <beatriz.cperez@sptech.school>
2025-04-08 11:36:13 -0300

    refactor: conexão bd

--- sprint2/ArquiteturaComputacional/dat-acqu-ino/main.js
+++ sprint2/ArquiteturaComputacional/dat-acqu-ino/main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -23,7 +23,7 @@ const serial = async (
             user: 'USUARIO_DO_BANCO',
             password: 'SENHA_DO_BANCO',
             database: 'DATABASE_DO_BANCO',
-            port: 3306
+            port: 3307
         }
     ).promise();
 
@@ -64,10 +64,10 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
-                [sensorAnalogico, sensorDigital]
+                'INSERT INTO captura (sensorAnalogico) VALUES (?)',
+                [sensorAnalogico]
             );
-            console.log("valores inseridos no banco: ", sensorAnalogico + ", " + sensorDigital);
+            console.log("valores inseridos no banco: ", sensorAnalogico);
 
         }
 
