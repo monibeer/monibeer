199eccc8ed1fd506d17472bca136c173991a22ad
joaolureis <joao.lcarvalho@sptech.school>
2025-04-23 00:22:45 -0300

    refactor: data-acqu-ino

--- sprint2/arq-computacional/dat-acqu-ino/main.js
+++ sprint2/arq-computacional/dat-acqu-ino/main.js
@@ -1,14 +1,14 @@
 // importa os bibliotecas necessários
-const serialport = require("serialport");
+const serialport = require("serialport");/*---------------1° BLOCO------------------*/
 const express = require("express");
 const mysql = require("mysql2");
 
 // constantes para configurações
 const SERIAL_BAUD_RATE = 9600;
-const SERVIDOR_PORTA = 3300;
+const SERVIDOR_PORTA = 3300; 
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = true;
+const HABILITAR_OPERACAO_INSERIR = true; /*---------------2° BLOCO------------------*/
 
 // função para comunicação serial
 const serial = async (
@@ -25,32 +25,30 @@ const serial = async (
       port: 3307,
     })
     .promise();
-<<<<<<< HEAD
-=======
+
   // lista as portas seriais disponíveis e procura pelo Arduino
-  const portas = await serialport.SerialPort.list();
+  const portas = await serialport.SerialPort.list(); /*---------------3° BLOCO------------------*/
   const portaArduino = portas.find(
     (porta) => porta.vendorId == 2341 && porta.productId == 43
   );
   if (!portaArduino) {
     throw new Error("O arduino não foi encontrado em nenhuma porta serial");
   }
->>>>>>> 315f95722baae2482c7c2473bf64aee7c0f7d369
 
-  // configura a porta serial com o baud rate especificado
+  // configura a porta serial com o baud rate especificado /*---------------4° BLOCO------------------*/
   const arduino = new serialport.SerialPort({
     path: portaArduino.path,
     baudRate: SERIAL_BAUD_RATE,
   });
 
   // evento quando a porta serial é aberta
-  arduino.on("open", () => {
+  arduino.on("open", () => { 
     console.log(
       `A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`
     );
   });
 
-  // processa os dados recebidos do Arduino
+  // processa os dados recebidos do Arduino  /*---------------5° BLOCO------------------*/
   arduino
     .pipe(new serialport.ReadlineParser({ delimiter: "\r\n" }))
     .on("data", async (data) => {
@@ -65,7 +63,7 @@ const serial = async (
       // valoresSensorDigital.push(sensorDigital);
 
       // insere os dados no banco de dados (se habilitado)
-      if (HABILITAR_OPERACAO_INSERIR) {
+      if (HABILITAR_OPERACAO_INSERIR) {  /*---------------6° BLOCO------------------*/
         // este insert irá inserir os dados na tabela "medida"
         await poolBancoDados.execute(
           //'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
@@ -75,45 +73,15 @@ const serial = async (
         console.log("valores inseridos no banco: ", sensorAnalogico);
       }
     });
-<<<<<<< HEAD
 
-    // processa os dados recebidos do Arduino
-    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
-        console.log(data);
-        const valores = data.split(';');
-        // const sensorDigital = parseInt(valores[0]);
-        // const sensorAnalogico = parseFloat(valores[1]);
-        const sensorAnalogico = parseFloat(valores[0]);
-
-        // armazena os valores dos sensores nos arrays correspondentes
-        valoresSensorAnalogico.push(sensorAnalogico);
-        // valoresSensorDigital.push(sensorDigital);
-
-        // insere os dados no banco de dados (se habilitado)
-        if (HABILITAR_OPERACAO_INSERIR) {
-
-            // este insert irá inserir os dados na tabela "medida"
-            await poolBancoDados.execute(
-                //'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
-                'INSERT INTO captura (temperatura) VALUES (?)',
-                [sensorAnalogico]
-            );
-            console.log("valores inseridos no banco: ", sensorAnalogico );
-        }
-
-    });
-
-=======
-  
->>>>>>> 315f95722baae2482c7c2473bf64aee7c0f7d369
     // evento para lidar com erros na comunicação serial
-    arduino.on('error', (mensagem) => {
+    arduino.on('error', (mensagem) => { 
         console.error(`Erro no arduino (Mensagem: ${mensagem}`)
     });
 }
 
 // função para criar e configurar o servidor web
-const servidor = (
+const servidor = (  /*---------------7° BLOCO------------------*/
   valoresSensorAnalogico
   // valoresSensorDigital
 ) => {
