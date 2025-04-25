36b908f750e88b1218d539fb489fcc8e9022883a
joaolureis <joao.lcarvalho@sptech.school>
2025-04-24 21:36:00 -0300

    refactor: dashboard-individual

--- sprint2/public/js/chartJs-dashboard.js
+++ sprint2/public/js/chartJs-dashboard.js
@@ -7,13 +7,6 @@ new Chart(dataGraphicMediaTempIPA, {
         labels: ['Fermentadora 1','Fermentadora 2','Fermentadora 3','Fermentadora 4','Fermentadora 5','Fermentadora 6','Fermentadora 7','Fermentadora 8', 'Fermentadora 9', 'Fermentadora 10'],
         datasets: [
             {
-                label: 'Temp. Linha',
-                data: [19.2, 19.1, 21.7, 21.3, 19.6, 19.4, 20.2, 21.1, 19.7, 22.0],
-                borderWidth: 2,
-                borderColor: 'brown',
-                backgroundColor: 'brown',
-            },
-            {
                 label: 'Temperatura',
                 data: [19.2, 19.1, 21.7, 21.3, 19.6, 19.4, 20.2, 21.1, 19.7, 22.0],
                 borderWidth: 2,
@@ -67,7 +60,7 @@ new Chart(dataGraphicMediaTempIPA, {
 new Chart(dataGraphicIndividualTempIPA, {
     type: 'line',
     data: {
-        labels: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'],
+        labels: ['12:00', '12:01', '12:02', '12:03', '12:04', '12:05', '12:06', '12:07', '12:08', '12:09', '12:10'],
         datasets: [{
             label: 'Temperatura',
             data: [18, 19, 22, 21, 19, 19, 20, 21, 18, 22, 21],
